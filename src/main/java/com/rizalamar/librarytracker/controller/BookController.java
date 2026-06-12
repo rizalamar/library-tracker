2d82e4981c68a0b651c6f976d634853388617bd1
Rizal amarulloh <rizalamarulloh2014@gmail.com>
2026-06-12 13:08:11 +0700

    fix: return book method controller with ResponseEnity

--- src/main/java/com/rizalamar/librarytracker/controller/BookController.java
+++ src/main/java/com/rizalamar/librarytracker/controller/BookController.java
@@ -7,6 +7,7 @@ import com.rizalamar.librarytracker.service.BookService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
@@ -20,60 +21,69 @@ public class BookController {
     private final BookService bookService;
 
     @GetMapping
-    public WebResponse<List<BookResponse>> getAllBooks(){
+    public ResponseEntity<WebResponse<List<BookResponse>> > getAllBooks(){
         List<BookResponse> allBooks = bookService.getAllBooks();
-        return WebResponse.<List<BookResponse>>builder()
+        return ResponseEntity.ok(
+                WebResponse.<List<BookResponse>>builder()
                 .code(HttpStatus.OK.value())
                 .status("OK")
                 .data(allBooks)
-                .build();
+                .build()
+        );
     }
 
     @GetMapping("/{bookId}")
-    public WebResponse<BookResponse> getBookById(@PathVariable("bookId") UUID id){
+    public ResponseEntity<WebResponse<BookResponse>> getBookById(@PathVariable("bookId") UUID id){
         BookResponse bookResponse = bookService.getById(id);
-        return WebResponse.<BookResponse>builder()
-                .code(HttpStatus.OK.value())
-                .status("OK")
-                .data(bookResponse)
-                .build();
+        return ResponseEntity.ok(
+                WebResponse.<BookResponse>builder()
+                        .code(HttpStatus.OK.value())
+                        .status("OK")
+                        .data(bookResponse)
+                        .build()
+        );
     }
 
     @PostMapping
     @PreAuthorize("hasRole('ADMIN')")
-    @ResponseStatus(HttpStatus.CREATED)
-    public WebResponse<BookResponse> createBook(@Valid @RequestBody BookRequest request){
+    public ResponseEntity<WebResponse<BookResponse>> createBook(@Valid @RequestBody BookRequest request){
         BookResponse book = bookService.createBook(request);
-        return WebResponse.<BookResponse>builder()
-                .code(HttpStatus.CREATED.value())
-                .status("CREATED")
-                .data(book)
-                .build();
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .body(
+                        WebResponse.<BookResponse>builder()
+                                .code(HttpStatus.CREATED.value())
+                                .status("CREATED")
+                                .data(book)
+                                .build()
+                );
     }
 
     @PutMapping("/{bookId}")
     @PreAuthorize("hasRole('ADMIN')")
-    public WebResponse<BookResponse> updateBook(
+    public ResponseEntity<WebResponse<BookResponse>> updateBook(
             @PathVariable("bookId") UUID id,
             @Valid @RequestBody BookRequest request
     ){
         BookResponse bookResponse = bookService.updateBook(id, request);
-        return WebResponse.<BookResponse>builder()
-                .code(HttpStatus.OK.value())
-                .status("OK")
-                .data(bookResponse)
-                .build();
+        return ResponseEntity.ok(
+                WebResponse.<BookResponse>builder()
+                        .code(HttpStatus.OK.value())
+                        .status("OK")
+                        .data(bookResponse)
+                        .build()
+        );
     }
 
     @DeleteMapping("/{bookId}")
     @PreAuthorize("hasRole('ADMIN')")
-    @ResponseStatus(HttpStatus.NO_CONTENT)
-    public WebResponse<String> deleteBook(@PathVariable("bookId") UUID id){
+    public ResponseEntity<WebResponse<String>> deleteBook(@PathVariable("bookId") UUID id){
         bookService.deleteBook(id);
-        return WebResponse.<String>builder()
-                .code(HttpStatus.OK.value())
-                .status("OK")
-                .data("Book deleted successful")
-                .build();
+        return ResponseEntity.ok(
+                WebResponse.<String>builder()
+                        .code(HttpStatus.OK.value())
+                        .status("OK")
+                        .data("Book deleted successful")
+                        .build()
+        );
     }
 }
