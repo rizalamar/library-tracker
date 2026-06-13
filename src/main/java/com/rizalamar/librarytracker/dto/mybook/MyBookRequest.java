68397d8433a5adc2bf4503b3dc89ad31e346f053
Rizal amarulloh <rizalamarulloh2014@gmail.com>
2026-06-13 16:14:08 +0700

    refactor: update MyBookRequest

--- src/main/java/com/rizalamar/librarytracker/dto/mybook/MyBookRequest.java
+++ src/main/java/com/rizalamar/librarytracker/dto/mybook/MyBookRequest.java
@@ -1,5 +1,6 @@
 package com.rizalamar.librarytracker.dto.mybook;
 
+import com.rizalamar.librarytracker.domain.ReadingStatus;
 import com.rizalamar.librarytracker.dto.book.BookRequest;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotEmpty;
@@ -7,22 +8,7 @@ import jakarta.validation.constraints.NotEmpty;
 import java.util.List;
 
 public record MyBookRequest(
-        @NotBlank(message = "Title is required")
-        String title,
-
-        @NotEmpty(message = "Author is required")
-        List<BookRequest.Author> authors,
-
-        @NotBlank(message = "Isbn is Required")
-        String isbn,
-
-        String subtitle,
-
-        @NotEmpty(message = "Publisher us required")
-        List<BookRequest.Publisher> publishers,
-        String publishedDate,
-        String imageUrl
+    ReadingStatus status,
+    String notes
 ) {
-    public record Author(String url, String name){}
-    public record Publisher(String name){}
 }
