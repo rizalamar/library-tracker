ab815c513b4f4894c85c1a4f769d388ba2afc82c
Rizal amarulloh <rizalamarulloh2014@gmail.com>
2026-08-27 22:44:30 +0700

    refactor: add CORS Configuration and update PORT in app.props

--- src/main/java/com/rizalamar/librarytracker/config/Webconfig.java
+++ src/main/java/com/rizalamar/librarytracker/config/Webconfig.java
@@ -4,6 +4,7 @@ import com.rizalamar.librarytracker.security.CurrentUserArgumentResolver;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.util.List;
@@ -16,5 +17,13 @@ public class Webconfig implements WebMvcConfigurer {
     @Override
     public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
         resolvers.add(currentUserArgumentResolver);
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*");
     }
 }
