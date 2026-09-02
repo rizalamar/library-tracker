7c78230e5d0425d4e95ad879299aafebf40766d2
Rizal amarulloh <rizalamarulloh2014@gmail.com>
2026-09-02 08:33:53 +0700

    fix: remove unused import

--- src/main/java/com/rizalamar/librarytracker/service/OpenLibraryService.java
+++ src/main/java/com/rizalamar/librarytracker/service/OpenLibraryService.java
@@ -10,7 +10,6 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.server.ResponseStatusException;
 
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
