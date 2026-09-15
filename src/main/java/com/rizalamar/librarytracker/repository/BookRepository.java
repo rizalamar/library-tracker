a5af21a3b0192dc2eb9f7fbbd1b975dc23ac8a96
Rizal amarulloh <rizalamarulloh2014@gmail.com>
2026-09-15 19:10:56 +0700

    fix: overriding method default by adding EntityGraph Annotation

--- src/main/java/com/rizalamar/librarytracker/repository/BookRepository.java
+++ src/main/java/com/rizalamar/librarytracker/repository/BookRepository.java
@@ -1,11 +1,17 @@
 package com.rizalamar.librarytracker.repository;
 
 import com.rizalamar.librarytracker.domain.Book;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.UUID;
 
 @Repository
 public interface BookRepository extends JpaRepository<Book, UUID> {
+
+    @Override
+    @EntityGraph(attributePaths = {"authors", "publishers"})
+    List<Book> findAll();
 }
